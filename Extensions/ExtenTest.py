8c40fabb027ebebe5683f57a7941e3bbc6737a83
MrGreen <mrgreengames@protonmail.com>
2019-06-14 19:59:23 +0800

    Add files via upload

---
+++ Extensions/ExtenTest.py
@@ -0,0 +1,17 @@
+# A test extension for Smart Interactive App
+# Created by Mr Green
+
+
+def key():
+    """Returns the key needed for SIA to validate the extension"""
+    keY = 1  # Replace 1 with your key
+    return keY
+
+
+def run():
+    """The part that will run when SIA calls for the extension."""
+    print('Welcome to text extension!')
+    print('Created by Mr Green from GreenGames.')
+    print('This will test if SIA\'s extension installer and runner works!')
+    print('If you are seeing this then it does work!')
+    print('Congrats!')
